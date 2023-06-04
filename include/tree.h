044a08a0b88d25fc1519021b46e0a746742aaa36
KantinovV <125280143+KantinovV@users.noreply.github.com>
2023-06-05 00:01:09 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -8,14 +8,14 @@ class Tree {
  private:
     struct Node {
         char value;
-        std::vector<Node*> haguvagi;
+        std::vector<Node*> childs;
     };
     Node* root;
  public:
-    int per;
-    int ner;
-    std::vector<char> dota;
-    std::vector<char> temp_data;
+    int per_size;
+    int n_fact;
+    std::vector<char> data;
+    std::vector<char> tmp_data;
     std::vector<char> v;
     int fact(int n) {
         int k = 1;
@@ -24,19 +24,19 @@ class Tree {
         }
         return k;
     }
-    explicit Tree(std::vector<char> in): root(nullptr), ner(0), per(0) {
+    explicit Tree(std::vector<char> in): root(nullptr), n_fact(0), per_size(0) {
         if (root) {
-            throw std::string("haguvagi!");
+            throw std::string("Error!");
         } else {
             if (!in.empty()) {
                 v = in;
-                per = in.size();
-                ner = fact(per);
+                per_size = in.size();
+                n_fact = fact(per_size);
                 root = new Node;
                 root->value = '*';
                 for (int i = 0; i < in.size(); i++) {
-                    root->haguvagi.push_back(new Node);
-                    root->haguvagi[i]->value = in[i];
+                    root->childs.push_back(new Node);
+                    root->childs[i]->value = in[i];
                 }
                 for (int i = 0; i < in.size(); i++) {
                     std::vector<char> in_i;
@@ -44,34 +44,34 @@ class Tree {
                        if (k != i) {
                            in_i.push_back(in[k]);
                        } else {
-                           dota.push_back(in[i]);
+                           data.push_back(in[i]);
                        }
                     }
                     if (!in_i.empty()) {
                         root->childs[i] = addChilds(root->childs[i], in_i);
                     }
                     for (int q = 0; q < in.size() - 1; q++) {
-                        temp_data.push_back(dota[dota.size() - 1]);
-                        dota.pop_back();
+                        tmp_data.push_back(data[data.size() - 1]);
+                        data.pop_back();
                     }
                     data.push_back(in[i]);
                     for (int q = 0; q < in.size() - 1; q++) {
                         data.push_back(tmp_data[tmp_data.size() - 1]);
-                        temp_data.pop_back();
+                        tmp_data.pop_back();
                     }
-                    temp_data.clear();
+                    tmp_data.clear();
                 }
             }
         }
     }
     Node* addChilds(Node* root, std::vector<char> ch) {
         if (!root) {
-            throw std::string("haguvagi!");
+            throw std::string("Error!");
         } else {
             if (!ch.empty()) {
                 for (int j = 0; j < ch.size(); j++) {
-                    root->haguvagi.push_back(new Node);
-                    root->haguvagi[j]->value = ch[j];
+                    root->childs.push_back(new Node);
+                    root->childs[j]->value = ch[j];
                 }
                 for (int j = 0; j < ch.size(); j++) {
                     std::vector<char> in_j;
@@ -79,11 +79,11 @@ class Tree {
                         if (k != j) {
                             in_j.push_back(ch[k]);
                         } else {
-                            dota.push_back(ch[j]);
+                            data.push_back(ch[j]);
                         }
                     }
                     if (!in_j.empty()) {
-                        root->haguvagi[j] = addChilds(root->haguvagi[j], in_j);
+                        root->childs[j] = addChilds(root->childs[j], in_j);
                     }
                 }
             }
