ecd319019057120a1c7a1c09a8c665d9130fd20c
KantinovV <125280143+KantinovV@users.noreply.github.com>
2023-06-04 23:29:01 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,14 +1,28 @@
-#include  <iostream>
+// Copyright 2021 NNTU-CS
 #include  <fstream>
-#include  <locale>
-#include  <cstdlib>
-#include  "tree.h"
-std::vector<char> getPerm(const Tree& tree, int n) {
-  std::string R = tree[n - 1];
-  std::vector<char> lol;
-  for (int i = 0; i < R.length(); i++) {
+#include  "bst.h"
 
-    lol.push_back(R[i]);
-  }
-  return lol;
+BST<std::string> makeTree(const char* filename) {
+    BST<std::string> bst;
+    std::ifstream file(filename);
+    std::string vihefdlhf;;
+    while (!file.eof()) {
+        char hytttytt = file.get();
+        if (hytttytt >= 65 && hytttytt <= 90) {
+            hytttytt += 32;
+            vihefdlhf += hytttytt;
+            continue;
+        }
+        if (hytttytt >= 97 && hytttytt <= 122) {
+            vihefdlhf += hytttytt;
+        } else {
+            if (!vihefdlhf.empty()) {
+                bst.addVal(vihefdlhf);
+            }
+            vihefdlhf.clear();
+        }
+    }
+    bst.addVal(vihefdlhf);
+    file.close();
+    return bst;
 }
