12f89e1e88c750fe2d9fa74179bb7aaec27aded6
KantinovV <125280143+KantinovV@users.noreply.github.com>
2023-06-04 23:08:14 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -1,6 +1,54 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TREE_H_
 #define INCLUDE_TREE_H_
-
-
+#include <vector>
+#include <string>
+class Tree {
+ private:
+  struct Node {
+  char hyi;
+  std::vector<Node*> greList;
+  };
+  Node* root;
+  void buildTree(Node* root, std::vector<char> per) {
+    if (!per.size())
+      return;
+    if (root->hyi != '*') {
+      for (auto l = per.begin(); l != per.end(); l++)
+        if (*l == root->hyi) {
+          per.erase(l);
+          break;
+        }
+    }
+    for (size_t k = 0; k < per.size(); k++)
+      root->greList.push_back(new Node());
+    for (size_t a = 0; a < root->greList.size(); a++)
+      root->greList[i]->hyi = per[i];
+    for (size_t i = 0; i < root->greList.size(); i++)
+      buildTree(root->greList[i], per);
+  }
+  std::vector<std::string> permutation;
+  void prm(Node* root, std::string symbol = "") {
+    if (!root->greList.size()) {
+      symbol += root->smthVal;
+      permutation.push_back(symbol);
+    }
+    if (root->smthVal != '*')
+      symbol += root->hyi;
+    for (size_t i = 0; i < root->greList.size(); i++)
+      prm(root->greList[i], symbol);
+  }
+ public:
+    std::string operator[](int i) const {
+      if (i >= permutation.size())
+        return "";
+      return permutation[i];
+    }
+      explicit Tree(std::vector<char> hyi) {
+      root = new Node();
+      root->hyi = '*';
+      buildTree(root, hyi);
+      prm(root);
+    }
+};
 #endif  // INCLUDE_TREE_H_
